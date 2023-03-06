6b37b7c6daf4f58e42ece4d60cceeb302e6158b0
Wesley Coutinho Correa da Silva <wesleycs@me.local>
2023-03-06 18:47:14 -0300

    Comitando sistema de automação de teste para o GitHub

---
+++ cypress/integration/LojaIntera/cadastrar.spec.js
@@ -0,0 +1,51 @@
+describe('Login loja intera', () =>{
+    
+
+    beforeEach(() => {
+        cy.visit('https://qastoredesafio.lojaintegrada.com.br/')
+    })
+
+    it('Verifica mensagem de erro ao clicar no botao cadastrar sem passar email', () => {
+        cy.contains('a', 'Minha Conta').click();
+        cy.contains('button', 'Cadastrar').click();
+        cy.contains('div', 'Email não informado, digite seu email no campo para cadastro.').should('be.visible');
+    })
+
+    it('Validar campo de adicionar um email a ser cadastrado', () => {
+        cy.contains('a', 'Minha Conta').click();
+        cy.get('fieldset > div > #id_email').type('teste-wesley@gmail.com');
+        cy.contains('button', 'Cadastrar').click();
+    })
+
+    it('Validar criacao de um usuario ja criado', () => {
+        cy.contains('a', 'Minha Conta').click();
+        cy.get('fieldset > div > #id_email').type('teste-wesley2@gmail.com');
+        cy.contains('button', 'Cadastrar').click();
+        cy.contains('div', 'Já existe um cliente cadastrado com este email.').should('be.visible');    
+    })
+
+    const usuario = require('../../fixtures/usuarios.json')
+    usuario.forEach(usuario => {
+
+        it.only(`Validar criacao de um novo usuario ${usuario.nome} `, () => {
+            cy.contains('a', 'Minha Conta').click();
+            cy.get('fieldset > div > #id_email').type(usuario.email);
+            cy.contains('button', 'Cadastrar').click();
+            cy.get('#id_confirmacao_email').type(usuario.email);
+            cy.get('#id_senha').type(usuario.senha);
+            cy.get('#id_cep').type(usuario.cep);
+            cy.get('#id_numero').type(usuario.numerocasa);
+            cy.get('#id_confirmacao_senha').type(usuario.senha);
+            cy.get('#id_nome').type(usuario.nome);
+            cy.get('#id_cpf').type(usuario.cpf);
+            cy.get('#id_telefone_celular').type(usuario.celular);
+            cy.get('#id_telefone_principal').type(usuario.telefone);
+            cy.get('#id_sexo').as('sexo').select('Masculino');
+            cy.get('#id_data_nascimento').type(usuario.datanascimento);
+            cy.contains('button', 'Criar Conta').click();
+        })
+
+    });
+
+    
+})
---
+++ cypress/integration/LojaIntera/login.spec.js
@@ -0,0 +1,59 @@
+describe('Login loja intera', () =>{
+
+    beforeEach(() => {
+        cy.visit('https://qastoredesafio.lojaintegrada.com.br/')
+    })
+    
+    it('Verificar login passando senha menor que 5 caractere, validar mensagem de erro', () => {
+        cy.contains('a', 'Minha Conta').click();
+        cy.get('.controls > #id_email').type('teste-001@gmail.com');
+        cy.get('#id_senha').type('123');
+        
+        cy.contains('button', 'Prosseguir').click();
+        cy.contains('li', 'Certifique-se de que o valor tenha no mínimo 5 caracteres (ele possui 3).').should('be.visible');
+    })
+
+    it('Verificar login passando o email invalido', () => {
+        cy.contains('a', 'Minha Conta').click();
+        cy.get('.controls > #id_email').type('teste-wesley@gmail.com');
+        cy.get('#id_senha').type('123456');
+        cy.contains('button', 'Prosseguir').click();
+        cy.contains('div', 'Não foi possível entrar pois o email ou senha não conferem. Por favor tente novamente com outro email ou senha.').should('be.visible');
+    })
+
+    it('Verificar login passando a senha invalida', () => {
+        cy.contains('a', 'Minha Conta').click();
+        cy.get('.controls > #id_email').type('teste-001@gmail.com');
+        cy.get('#id_senha').type('1234sdfa');
+        cy.contains('button', 'Prosseguir').click();
+        cy.contains('div', 'Não foi possível entrar pois o email ou senha não conferem. Por favor tente novamente com outro email ou senha.').should('be.visible');
+    })
+
+    it('Verificar login passando usuario e senha correta', () => {
+        cy.contains('a', 'Minha Conta').click();
+        cy.get('.controls > #id_email').type('teste-001@gmail.com');
+        cy.get('#id_senha').type('123456');
+        cy.contains('button', 'Prosseguir').click();
+        cy.get('.btn-group > .botao').should('be.visible');
+    })
+
+    it('Validar login e logout', () => {
+        cy.contains('a', 'Minha Conta').click();
+        cy.get('.controls > #id_email').type('teste-001@gmail.com');
+        cy.get('#id_senha').type('123456');
+        cy.contains('button', 'Prosseguir').click();
+        cy.get('.btn-group > .botao').should('be.visible');
+        cy.contains('a', 'Olá, Teste Wesley').click();
+        cy.get('.menu-user-logout').click();
+    })
+
+    it('Verificar login não passando valores de senha e email', () => {
+        cy.contains('a', 'Minha Conta').click();
+        cy.contains('button', 'Prosseguir').click();
+        cy.get(':nth-child(2) > .controls > .errorlist > li').should('be.visible');
+        cy.get(':nth-child(3) > .controls > .errorlist > li').should('be.visible');
+    })
+
+
+
+})
